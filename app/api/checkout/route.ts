8c029fcb896ee99bf271611c25e7a1f85d797706
redraf97 <rafikzem1@gmail.com>
2026-01-29 14:53:59 +0100

    Mise a jour img

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -7,7 +7,7 @@ if (!process.env.STRIPE_SECRET_KEY) {
 }
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: '2025-01-27.acacia', // Utilise la version suggérée par VS Code
+  apiVersion: '2025-12-15.clover',
 });
 
 export async function POST(request: Request) {
